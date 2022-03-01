cf5b0153672b2415f1140b3b3ea1f57a42cd1ef8
Regan Dsouza <65179697+rtp77@users.noreply.github.com>
2022-03-01 19:54:21 +0400

    Acceleration * Time

--- Gravity on a 1D body/G1.cpp
+++ Gravity on a 1D body/G1.cpp
@@ -1,3 +1,7 @@
+//Simple simulation of a body freefalling and hitting the ground
+//The equation used to achive velocity of the body is V = U + a*T
+//This code does not account for pixel height or any length conversion
+
 #include<iostream>
 #include<SFML/Graphics.hpp>
 
@@ -8,7 +12,7 @@ int main(int argc, char** argv[]) {
 	bool aiming = false;
 	bool action = false;
 	float velocity = 0;
-	float acceleration = 9.81;
+	float acceleration = 9.81; //value isnt modified to fit into pixel height
 
 	sf::Clock clock;
 
@@ -22,6 +26,7 @@ int main(int argc, char** argv[]) {
 			if (e.type == sf::Event::Closed) {
 				window.close();
 			}
+			//Drag and Drop of ball
 			else if (e.type==sf::Event::MouseButtonPressed and e.type!=sf::Event::MouseButtonReleased) {
 				aiming = true;
 				action = false;
@@ -42,7 +47,7 @@ int main(int argc, char** argv[]) {
 		if (action == true) {
 			sf::Time t = clock.getElapsedTime();
 			velocity = acceleration * t.asSeconds();
-			if (ball.getPosition().y >= (window.getSize().y - 42)) { action = false; }
+			if (ball.getPosition().y >= (window.getSize().y - 42)) { action = false; } //Boundary
 		}
 
 		//Action
